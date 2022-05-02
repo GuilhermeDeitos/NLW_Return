d9ba86bf1b3d7c8bd5af9507fe6a2fd1c987318c
Guilherme Deitos <guilherme.cascavel@gmail.com>
2022-05-02 16:24:07 -0300

    adicionando tailwindcss no projeto

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,12 +4,12 @@ interface ButtonProps {
 
 
 function Button(props: ButtonProps){
-  return <button>{props.text}</button>
-}
+  return <button className="bg-[#8262e7] p-2 px-4 h-10 rounded text-violet-200 hover:bg-violet-800 transition-colors">{props.text}</button> //p = padding e 2 = 8px(0.5rem)
+} //px = padding no eixo x
 
 function App() {
   return (
-    <div>
+    <div className="flex gap-2 m-2"> 
       <Button text="Enviar" />
       <Button text="Ok"/>
 
