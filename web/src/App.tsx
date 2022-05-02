ea60e28074eecf384c648ccf0f3772323c5eb469
Guilherme Deitos <guilherme.cascavel@gmail.com>
2022-05-02 16:46:55 -0300

    Estados e botão funcional

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,9 @@
-interface ButtonProps {
-  text: string; // text ?: string para deixar opcional
-}
-
-
-function Button(props: ButtonProps){
-  return <button className="bg-[#8262e7] p-2 px-4 h-10 rounded text-violet-200 hover:bg-violet-800 transition-colors">{props.text}</button> //p = padding e 2 = 8px(0.5rem)
-} //px = padding no eixo x
+import { Widget } from "./components/Widget"
 
 function App() {
   return (
     <div className="flex gap-2 m-2"> 
-      <Button text="Enviar" />
-      <Button text="Ok"/>
-
-
+      <Widget/>
     </div>
   )
 }
